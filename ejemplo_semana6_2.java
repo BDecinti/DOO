ce86eaab7b472b90cf0976a72ec3acce5c7784c8
BDecinti <br.decinti@duocuc.cl>
2025-09-25 20:16:04 -0300

    Update ejemplo_semana6_2.java

--- ejemplo_semana6_2.java
+++ ejemplo_semana6_2.java
@@ -1,4 +1,4 @@
-  /*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
@@ -19,17 +19,70 @@ public class Ejemplo_semana6_2 {
         
         Scanner sc = new Scanner(System.in);
         
-        List<Persona> listaPersona = new ArrayList();
+        List<Persona> listaPersonas = new ArrayList();
         
         int opMenu = 0;
         do {
-            System.out.println("1.");
-            System.out.println("1.");
-            System.out.println("1.");
-            System.out.println("1.");
-            System.out.println("1.");
-            System.out.println("1.");
-        }while (true);
+            System.out.println("#################################");
+            System.out.println("#####   Seleccione Opcion  ######");
+            System.out.println("#################################");
+            System.out.println("1. Crear Persona");
+            System.out.println("2. Buscar una Persona");
+            System.out.println("3. Listar todas las Personas");
+            System.out.println("4. Editar una Personas");
+            System.out.println("5. Eliminar persona");
+            System.out.println("6. Salir");
+            
+            opMenu = Integer.parseInt(sc.nextLine());
+            
+            
+            switch(opMenu){
+                case 1:
+                    
+                    System.out.println("Ingrese el rut de la persona: ");
+                    String rut = sc.nextLine();
+                    System.out.println("Ingrese Nombre de la Persona: ");
+                    String nombre = sc.nextLine();
+                    System.out.println("Ingrese edad de la Persona: ");
+                    int edad = Integer.parseInt(sc.nextLine());
+                    Persona persona = new Persona(edad, rut, nombre);
+                    listaPersonas.add(persona);
+                    
+                    
+                    break;
+                case 2:
+                    boolean encontrado = false;
+                    System.out.println("Ingrese el rut de la persona: ");
+                    String rutBuscado = sc.nextLine();
+                    for (Persona listaPersona : listaPersonas){
+                        if (listaPersona.getRut().equals(rutBuscado));
+                            System.out.println(listaPersona.toString());
+                            encontrado = true;
+                    }
+                    
+                    if (!encontrado){
+                        System.out.println("no se encontro nada");
+                    }
+                    
+                    break;
+                case 3:
+                    
+                    for (Persona personaListada : listaPersonas){
+                        System.out.println(personaListada.toString());
+                    }
+                    
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+                    System.out.println("Ingrese una opcion valida");
+            }
+            
+        }while (opMenu != 6);
         
     }
 }
